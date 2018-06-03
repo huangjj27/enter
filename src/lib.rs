5e6c237a454a07e1d84c004e7b8e878665c44bec
huangjj27 <huangjj.27@qq.com>
2018-06-03 15:58:16 +0800

    drop the traits and only provide the constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,13 @@
-//! This is a simple library for dealing with the CR, LF, CRLF problem.
-//! It provides a trait `Enter`, And a default implement for `str`
-//! that trim corresponding newline characters `ENTER` of running OS's stdin 
+//! The only thing the crate doing is to privide constant CR, LF, CRLF and ENTER.
 
-/// Definations of newline characters.  
-pub(crate) mod newlines {
-    #![allow(dead_code)]
+pub const CR: &'static str = "\r";
+pub const LF: &'static str = "\n";
+pub const CRLF: &'static str = "\r\n";
 
-    // MacOS now uses LF for newlines. Commented out.
-    // const CR: &'static str = "\r";
+/// newlines' character is CRLF. This indicate you're using Windows OS.
+#[cfg(target_os = "windows")]
+pub const ENTER: &'static str = CRLF;
 
-    const LF: &'static str = "\n";
-    const CRLF: &'static str = "\r\n";
-
-    /// newlines' character is CRLF. This indicate you're using Windows OS.
-    #[cfg(target_os = "windows")]
-    pub const ENTER: &'static str = CRLF;
-
-    /// newlines' character is LF. This indicate you're using non-Windows OS.
-    #[cfg(not(target_os = "windows"))]
-    pub const ENTER: &'static str = LF;
-}
-
-/// Constant representing newline characters in the target OS. 
-/// The mapping of newline characters is as follows:
-///
-/// - Windows -> CRLF
-/// - not Windows -> LF
-pub use newlines::ENTER;
-
-pub trait Enter {
-    fn enter(&self) -> &str;
-}
-
-impl Enter for str {
-    /// trimming corresponding newline characters of running OS's stdin.
-    ///
-    /// # example(s)
-    /// 
-    /// ```rust
-    /// use enter::{ Enter, ENTER };
-    /// 
-    /// let mut str_with_enter = "Hello, World!".to_string();
-    /// str_with_enter.push_str(ENTER);
-    /// assert_eq!(str_with_enter.enter(), "Hello, World!");
-    /// ```
-    fn enter(&self) -> &str {
-        self.trim_right_matches(ENTER)
-    }
-}
+/// newlines' character is LF. This indicate you're using non-Windows OS.
+#[cfg(not(target_os = "windows"))]
+pub const ENTER: &'static str = LF;
